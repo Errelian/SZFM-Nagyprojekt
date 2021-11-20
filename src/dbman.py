b988fc776b9005c6c1244fa80f69887c3eaa0a34
Pbalazs101 <42806085+Pbalazs101@users.noreply.github.com>
2021-11-20 14:13:02 +0100

    New user creation method added

--- src/dbman.py
+++ src/dbman.py
@@ -1,10 +1,16 @@
+#!/usr/bin/env python3
+
 import sqlite3
+import ChessBot
+import os.path
 
-conn = sqlite3.connect('scores.db')
+database_exists = os.path.isfile("scores.db")
 
-c = conn.cursor()
 
-c.execute("""CREATE TABLE results (
+def create_database():
+
+    c.execute("""CREATE TABLE results (
+    id integer not null primary key,
     player_id text,
     chess_games integer,
     chess_win integer,
@@ -16,10 +22,56 @@ c.execute("""CREATE TABLE results (
     ttt_lose integer
     )""")
 
-c.execute("INSERT INTO results VALUES ('AcesHigh37', '15', '7','3','5','30','5','20','5')")
+
+def get_next_id():
+    c.execute("SELECT id FROM results ORDER BY ID DESC LIMIT 1")
+    try:
+        current_id = c.fetchone()[0]
+    except Exception as e:
+        print("Empty database")
+        return 0
+    else:
+        return current_id + 1
+
+
+
+def create_new_user():
+
+    c.execute("INSERT INTO results VALUES (?, ?, '0', '0','0','0','0','0','0','0')", (get_next_id(),user_id,) )
+
+
+
+
+conn = sqlite3.connect('scores.db')
+c = conn.cursor()
+
+if database_exists == False:
+    create_database()
+
+
+print(get_next_id())
+#user_id, type_of_game
+recieved_array = ["Aces","Chess"]
+user_id = recieved_array[0]
+game_type = recieved_array[1]
+
+c.execute("SELECT id FROM results WHERE player_id = ?", (user_id,))
+try:
+    c.fetchone()[0] == int
+except Exception as e:
+    print("Not found")
+    create_new_user()
+else:
+    print("Found")
+
+
 
 c.execute("SELECT * FROM results")
 print(c.fetchall())
 
+#print(id)
+
 conn.commit()
 conn.close()
+
+########################################################################################
